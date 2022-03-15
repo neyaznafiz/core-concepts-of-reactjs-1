e92939d2fb2c5e681e8f4804ddaa2bbb89d3044e
niiyazobug <niiyaz5555@gmail.com>
2022-03-15 22:01:52 +0600

    reactjs httml and css write concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Alex'
+const singer = { name: 'Dr. Mahfuz', job: 'Singer' }
+const singer2 = { name: 'Eva Rahman', job: 'Fimale Singer' }
+
+const singerStyle = {
+  color: 'yellow',
+  backgroundColor:'gray',
+  padding: '10px',
+  borderRadius: '10px'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div style={{border:'2px solid black', marginBottom:'20px', padding:'20px'}}>
+        <h1>JSX</h1>
+        <p>- javaScript XML.</p>
+        <p>- Allow us to write HTML in react.</p>
+        <p>- Writing and adding HTML in React is easier.</p>
+        </div>
+        
+
+        <div className="container">
+          <h3>Hello Dude! React. How are you</h3>
+        </div>
+
+        <div className="music">
+
+          <p> Name: {name + ' ' + 'Costa'} </p>
+
+          <p style={singerStyle}> Name: {singer.name}. Job: {singer.job} </p>
+
+          <p style={{backgroundColor:'gray', padding: '10px',  borderRadius: '10px'}}> Name: {singer2.name}. Job: {singer2.job} </p>
+
+        </div>
+
+       
+      
     </div>
   );
 }
